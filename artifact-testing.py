c71550218970671b5f8ce6f20bb5cb2ff90dd1ca
Daniel Sissman <dsissman@getty.edu>
2019-04-18 12:48:02 -0700

    Fixed script for Python 3 compatibility

--- artifact-testing.py
+++ artifact-testing.py
@@ -8,11 +8,13 @@ from cromulent.model import factory, \
 	Acquisition, ManMadeFeature, VisualItem, Set, \
 	PropositionalObject, Payment, Creation, Phase
 
+# TODO clarify how we add an ordering to the alternatite titles and other data attributes where ordering is important
+
 o = Object("Irises");
 p = Person("Van Gogh");
 
 # Set Object URI
-o.id = "https://data.getty.edu/museum/object/826/";
+o.id = "https://data.getty.edu/museum/collection/object/826/";
 
 # Set Classified As
 o.classified_as = []
@@ -32,31 +34,43 @@ o.classified_as.append(t);
 # Set Accession Number
 o.identified_by = []
 i = Identifier();
-i.id = "https://data.getty,edu/museum/identifier/?1"
+i.id = "https://data.getty,edu/museum/collection/identifier/?1"
 i.content = "90.PA.20";
 i._label = "Accession Number"
 i.classified_as = []
+
+# Set Accession Number Classification
 t = Type()
 t._label = "Accession Number"
 t.id = "http://vocab.getty.edu/aat/300312355"
 i.classified_as.append(t)
 o.identified_by.append(i)
 
-# Set Title
+# Set Primary Title
 n = Name()
-n.id = "https://data.getty,edu/museum/name/?1"
+n.id = "https://data.getty,edu/museum/collection/name/?1"
 n.content = "Irises"
 n.classified_as = []
+
+# Set Primary Title Classification
+# This is important as it denotes the title as being the primary out of any titles associated with the work
 t = Type()
 t.id = "http://vocab.getty.edu/aat/300404670"
 t._label = "Primary Title"
 n.classified_as.append(t)
 o.identified_by.append(n)
 
+# Add any Alternate Title(s); in this case just one (as an example)
 n = Name()
-n.id = "https://data.getty,edu/museum/name/?2"
+n.id = "https://data.getty,edu/museum/collection/name/?2"
 n.content = "IRISES"
 o.identified_by.append(n)
 
 
-print factory.toString(o, compact=False)
+print("Object Representation");
+print("#####################");
+print(factory.toString(o, compact=False))
+print("");
+print("Person Representation");
+print("#####################");
+print(factory.toString(p, compact=False))

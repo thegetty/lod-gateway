2e0b72fb2c74a0b9c47f91f311c153ad85f7ac71
Daniel Sissman <bluebinary@users.noreply.github.com>
2019-06-26 12:45:07 -0700

    Syntax Fixes for .env File; Additional Comments in startup.sh - DEV-1663

--- source/transformer/startup.sh
+++ source/transformer/startup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# determine the path to this script, regardless of where it is run from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
-# run the process
-echo "startup.sh path ${DIR}";
+echo "startup.sh path: ${DIR}";
 
+# run the process
 if [ "${DIR}" != "/" ]; then
+	# if running our script directly on the command line, import our environment variables
 	ENV="$( cd "$( dirname "${DIR}/../../.env" )" && pwd )/.env";
 	
-	/usr/bin/env $(grep -v '^#' ${ENV} | xargs) python3 ${DIR}/startup.py $@
+	# see https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-pair-values
+	# see https://unix.stackexchange.com/questions/26235/how-can-i-cat-a-file-and-remove-commented-lines
+	/usr/bin/env $(grep -v '^#' "${ENV}" | xargs) python3 "${DIR}/startup.py" $@;
 else
-	python3 ${DIR}/startup.py $@
+	# when running in Docker, our environment variables will be available through our docker-compose.yml
+	# or through the integrations with Hashicorp Vault in production
+	python3 "${DIR}/startup.py" $@;
 fi

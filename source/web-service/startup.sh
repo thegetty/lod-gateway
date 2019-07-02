b5d5d5e735b52986ecd284300baad4633312eeab
Daniel Sissman <bluebinary@users.noreply.github.com>
2019-07-02 12:39:24 -0700

    Docker for Windows Compatibility Fixes - DEV-1663
    Fixes related to Docker for Windows compatibility including the absence
    of the PWD shell variable.

--- source/web-service/startup.sh
+++ source/web-service/startup.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-export FLASK_ENV=development
-export FLASK_DEBUG=1
-export FLASK_APP=/app/startup.py
 
-flask run --host=0.0.0.0 --port=5100
+# determine the path to this script, regardless of where it is run from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+echo "startup.sh path: ${DIR}";
+
+# run the process...
+
+# if running our script directly on the command line (detected here by ${DIR} not being "/")...
+if [ "${DIR}" != "/" ]; then
+	echo "startup.sh: running locally"
+	
+	# import our environment variables from our .evn file...
+	ENV="$( cd "$( dirname "${DIR}/../../.env" )" && pwd )/.env";
+	
+	# see https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-pair-values
+	# see https://unix.stackexchange.com/questions/26235/how-can-i-cat-a-file-and-remove-commented-lines
+	/usr/bin/env $(grep -v '^#' "${ENV}" | xargs) FLASK_ENV=MART_FLASK_ENV FLASK_DEBUG=MART_FLASK_DEBUG FLASK_APP=MART_FLASK_APP flask run --host=0.0.0.0 --port=5100 $@;
+else # or when running in Docker...
+	echo "startup.sh: running within docker"
+	
+	# ...our environment variables will be available through docker-compose.yml or from Hashicorp Vault in production
+	flask run --host=0.0.0.0 --port=5100 $@
+fi

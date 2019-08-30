79e87cab31c414041ffaa9a24b17063fc8a3021a
Daniel Sissman <bluebinary@users.noreply.github.com>
2019-08-30 16:57:22 -0700

    Switch MART Application Server to uWSGI - DEV-2210
    Switched MART’s application server from Flask’s built-in development
    suited application server to the production ready uWSGI. This will
    provide performance improvements and resiliency for MART as well as a
    significant increase in throughput as uWSGI is multithreaded and can
    handle many requests simultaneously.

--- source/web-service/startup.sh
+++ source/web-service/startup.sh
@@ -5,19 +5,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 echo "startup.sh path: ${DIR}";
 
-# run the process...
-
-# if running our script directly on the command line (detected here by ${DIR} not being "/")...
-if [ "${DIR}" != "/" ]; then
-	# import our environment variables...
-	ENV="$( cd "$( dirname "${DIR}/../../.env" )" && pwd )/.env";
-	
-	# see https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-pair-values
-	# see https://unix.stackexchange.com/questions/26235/how-can-i-cat-a-file-and-remove-commented-lines
-	/usr/bin/env $(grep -v '^#' "${ENV}" | xargs) FLASK_ENV=MART_FLASK_ENV FLASK_DEBUG=MART_FLASK_DEBUG FLASK_APP=MART_FLASK_APP flask run --host=0.0.0.0 --port=5100 $@;
-else # or when running in Docker...
-	echo "startup.sh: running within docker"
-	
-	# ...our environment variables will be available through docker-compose.yml or from Hashicorp Vault in production
-	flask run --host=0.0.0.0 --port=5100 $@
-fi
+# run the process via uWSGI...
+uwsgi \
+	--http "0.0.0.0:${MART_WEB_SERVICE_PORT}" \
+	--manage-script-name \
+	--wsgi-file="${DIR}/startup.py" \
+	--callable "app" \
+	--master \
+	--processes=${MART_UWSGI_PROCESSES} \
+	--threads=${MART_UWSGI_THREADS} \
+	--thunder-lock \
+	$@

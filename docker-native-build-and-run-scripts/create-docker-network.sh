5ed80ff2469b18bfad2e4349098c8cdba831489b
David Newbury <david.newbury@gmail.com>
2019-11-17 10:18:49 -0800

    Remove docker-native scripts
    Remove code that appears to no longer be used.

--- docker-native-build-and-run-scripts/create-docker-network.sh
+++
@@ -1,1 +0,0 @@
-docker network create lod-gateway
--- docker-native-build-and-run-scripts/run-web-service.sh
+++
@@ -1,6 +0,0 @@
-docker run --network=lod-gateway --name=lod-gateway-web-service --rm -p 5100:5100 -d \
--e VAULT_ADDR="${VAULT_ADDR}" \
--e VAULT_TOKEN="${VAULT_TOKEN}" \
--e VAULT_ENV="${VAULT_ENV}" \
--e VAULT_APP_NAME="${VAULT_APP_NAME}" \
-lod-gateway-web-service:latest

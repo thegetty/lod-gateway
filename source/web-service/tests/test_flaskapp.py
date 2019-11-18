aeec06c6db8a6423db6583cc939887f079300470
David Newbury <david.newbury@gmail.com>
2019-11-18 07:47:22 -0800

    Improve flask tests
    This renames the test file to reflect the new file structure and improves
    the fixtures to allows for testing the Flask app directly.

--- source/web-service/tests/test_flaskapp.py
+++ source/web-service/tests/test_flaskapp.py
@@ -6,19 +6,25 @@ import pytest
 from flaskapp import create_app
 
 
-@pytest.fixture(scope="module")
-def test_client():
+@pytest.fixture
+def app():
     flask_app = create_app()
     flask_app.config["TESTING"] = True
-    testing_client = flask_app.test_client()
+    yield flask_app
 
-    ctx = flask_app.app_context()
+
+@pytest.fixture
+def client(app):
+
+    testing_client = app.test_client()
+
+    ctx = app.app_context()
     ctx.push()
     yield testing_client  # this is where the testing happens!
     ctx.pop()
 
 
-def test_home_page(test_client):
-    response = test_client.get("/")
+def test_home_page(client):
+    response = client.get("/")
     assert response.status_code == 200
     assert b"Welcome to the Getty's Linked Open Data Gateway Service" in response.data

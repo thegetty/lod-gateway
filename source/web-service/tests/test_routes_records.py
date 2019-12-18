57c730132f568960de9ae44a229fa0092df53f0f
Daniel Sissman <bluebinary@users.noreply.github.com>
2019-12-17 18:22:48 -0800

    Fix Last-Modified Header – DEV-3329
    Updated `Last-Modified` header tests in accordance with fixes for the
    handling of the `Last-Modified` header.

--- source/web-service/tests/test_routes_records.py
+++ source/web-service/tests/test_routes_records.py
@@ -4,6 +4,8 @@ import pytest
 
 from flaskapp.models import db, Record
 
+from datetime import datetime, timezone
+
 
 class TestObtainRecord:
     def test_typical_functionality(self, sample_data, client):
@@ -32,7 +34,18 @@ class TestObtainRecord:
 
     def test_last_modified(self, sample_data, client):
         response = client.get(f"/museum/collection/object/{sample_data['record'].uuid}")
-        last_modified = sample_data["record"].datetime_updated.strftime(
+
+        last_modified_date = None
+
+        if sample_data["record"].datetime_updated:
+            last_modified_date = sample_data["record"].datetime_updated
+        elif sample_data["record"].datetime_created:
+            last_modified_date = sample_data["record"].datetime_created
+
+        assert isinstance(last_modified_date, datetime)
+
+        last_modified = last_modified_date.astimezone(timezone.utc).strftime(
             "%a, %d %b %Y %H:%M:%S GMT"
         )
+
         assert response.headers["Last-Modified"] == last_modified

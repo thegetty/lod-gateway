0edfefce854a331e75161971ed141ffe509ee755
VGrabarchuk <vgrabarchuk@getty.edu>
2020-01-22 14:33:54 -0800

    reverted tests to env.example

--- source/web-service/tests/test_routes_ingest.py
+++ source/web-service/tests/test_routes_ingest.py
@@ -11,13 +11,11 @@ from uuid import uuid4
 
 class TestIngestRoute:
     def test_ingest_GET_not_allowed(self, client, current_app):
-        # namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
-        namespace = "museum/collection"
+        namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
         response = client.get(namespace + "/ingest")
         assert response.status_code == 405
 
     def test_ingest_POST(self, client, current_app):
-        # namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
-        namespace = "musemu/collection"
+        namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
         response = client.post(namespace + "/ingest")
         assert response.status_code == 200

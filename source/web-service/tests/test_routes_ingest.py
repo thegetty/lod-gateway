74ff97be4135e04b73a3c56e6a856ab86ab98c6f
VGrabarchuk <vgrabarchuk@getty.edu>
2020-01-22 13:33:02 -0800

    modified namespace in tests

--- source/web-service/tests/test_routes_ingest.py
+++ source/web-service/tests/test_routes_ingest.py
@@ -11,11 +11,13 @@ from uuid import uuid4
 
 class TestIngestRoute:
     def test_ingest_GET_not_allowed(self, client, current_app):
-        namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
+        # namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
+        namespace = "museum/collection"
         response = client.get(namespace + "/ingest")
         assert response.status_code == 405
 
     def test_ingest_POST(self, client, current_app):
-        namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
+        # namespace = current_app.config["DEFAULT_URL_NAMESPACE"]
+        namespace = "musemu/collection"
         response = client.post(namespace + "/ingest")
         assert response.status_code == 200

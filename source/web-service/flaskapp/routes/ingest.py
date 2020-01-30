92e434a733892dafebecce55c2061b19b867df9b
VGrabarchuk <vgrabarchuk@getty.edu>
2020-01-30 14:34:33 -0800

    Custom error added

--- source/web-service/flaskapp/routes/ingest.py
+++ source/web-service/flaskapp/routes/ingest.py
@@ -1,6 +1,8 @@
+import json
+
 from flask import Blueprint, request, abort
 
-from flaskapp.utilities import validate_ingest_record_set
+from collections import namedtuple
 
 
 # Create a new "ingest" route blueprint
@@ -26,8 +28,60 @@ def ingest_post():
     # Validate all records
     result = validate_ingest_record_set(record_list)
 
-    # For now return a string if success
+    # For now return a string if success and detailed error otherwise
     if result == True:
         return "success"
     else:
-        return abort(422)
+        return abort(
+            result[0].code,
+            description="Record on line " + str(result[1]) + ": " + result[0].detail,
+        )
+
+
+# Vallidation status named tuple. Note the same status code (e.g. '422')
+# can be used for different errors
+status_nt = namedtuple("name", "code detail")
+
+status_ok = status_nt(200, "Ok")
+status_wrong_syntax = status_nt(422, "Could not parse JSON record")
+status_id_missing = status_nt(422, "ID for the JSON record not found")
+
+
+# Validation functions
+def validate_ingest_record(rec):
+    """
+        Validate a single json record.
+        Check valid json syntax plus some other params      
+    """
+    try:
+        # if json syntax is good, validate other params
+        data = json.loads(rec)
+
+        # currently just the 'id'; in the future can be more
+        # check 'id' is present in the record
+        if "id" not in data.keys():
+            return status_id_missing
+
+        # return True if all validations passed, False - otherwise
+        return status_ok
+
+    except:
+        # json syntax is not valid
+        return status_wrong_syntax
+
+
+def validate_ingest_record_set(record_list):
+    """
+        Validate a list of json records. 
+        Break and return status if at least one record is invalid
+        Return line number where the error occured
+    """
+    counter = 0
+    for rec in record_list:
+        counter += 1
+        status = validate_ingest_record(rec)
+        if status != status_ok:
+            return (status, counter)
+
+    else:
+        return True

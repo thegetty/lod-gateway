8daf50ba85b4520062af991c06ebf702f30ee88e
VGrabarchuk <vgrabarchuk@getty.edu>
2020-01-31 10:50:22 -0800

    test_ingest_POST corrected

--- source/web-service/tests/test_routes_ingest.py
+++ source/web-service/tests/test_routes_ingest.py
@@ -15,7 +15,11 @@ class TestIngestRoute:
         response = client.get(f"/{ns}/ingest")
         assert response.status_code == 405
 
+    # Empty body should trigger 422 with 'no data' description
+    # Non empty request body is tested in 'validate_ingest' test
     def test_ingest_POST(self, client, current_app):
         ns = current_app.config["NAMESPACE"]
         response = client.post(f"/{ns}/ingest")
-        assert response.status_code == 200
+        assert response.status_code == 422
+        assert b"No input data found" in response.data
+

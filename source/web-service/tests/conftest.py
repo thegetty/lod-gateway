7a801d05b4e3bb9ab1cf0f770de276722aa126da
Daniel Sissman <bluebinary@users.noreply.github.com>
2020-03-18 22:38:59 -0700

    Improve Neptune Exception Handling – DEV-4211
    Moved the `requests_mocker()` fixture method from
    `test_routes_ingest.py` to `conftest.py`, and specified the
    `autouse=True` parameter so that all tests automatically utilize the
    requests mocking for accessing Neptune.

--- source/web-service/tests/conftest.py
+++ source/web-service/tests/conftest.py
@@ -1,4 +1,9 @@
+import json
 import pytest
+import os
+import re
+import requests
+import urllib.parse
 
 from datetime import datetime
 from uuid import uuid4
@@ -102,3 +107,74 @@ def sample_data(sample_record, sample_activity):
     record = sample_record()
     activity = sample_activity(record.id)
     return {"record": record, "activity": activity}
+
+
+@pytest.fixture(autouse=True)
+def requests_mocker(requests_mock):
+    """The `requests_mocker()` method supports mocking requests to Neptune, which is inaccessible
+    from within CircleCI. The `requests_mocker()` method provides support for mocking successful
+    HTTP requests to Neptune, and providing appropriate responses for the limited set of queries
+    performed by the /ingest endpoint's `process_neptune_record_set()` method, as well as support
+    for generating failed requests to mimic networking issues or connection time-outs."""
+
+    def mocker_text_callback(request, context):
+        print(request.url, request.path_url)
+
+        if request.path_url == "/status":
+            context.status_code = 200
+            return json.dumps({"status": "healthy",})
+        elif request.path_url == "/sparql":
+            sparql = None
+
+            if request.body.startswith("query=") or request.body.startswith("update="):
+                params = urllib.parse.parse_qsl(request.body)
+                if params:
+                    for param in params:
+                        if param[0] == "query" or param[0] == "update":
+                            sparql = param[1]
+                            break
+
+            if sparql:
+                if sparql.startswith("SELECT"):
+                    context.status_code = 200
+                    return json.dumps(
+                        {"results": {"bindings": [{"count": {"value": 0,}}],},}
+                    )
+                elif sparql.startswith("INSERT DATA"):
+                    context.status_code = 200
+                    return None
+                elif sparql.startswith("DROP GRAPH"):
+                    context.status_code = 200
+                    return None
+
+        context.status_code = 400
+        return None
+
+    neptune = os.getenv("NEPTUNE_ENDPOINT")
+
+    # Configure the default mock handlers; these rely on the `mocker_text_callback()` method defined above
+    pattern = re.compile(neptune.replace("/sparql", "/(.*)"))
+
+    requests_mock.head(pattern, text=mocker_text_callback)
+    requests_mock.get(pattern, text=mocker_text_callback)
+    requests_mock.post(pattern, text=mocker_text_callback)
+
+    # Configure the good mock handlers; these rely on the `mocker_text_callback()` method defined above
+    pattern = re.compile(
+        neptune.replace("http://", "mock-pass://").replace("/sparql", "/(.*)")
+    )
+
+    requests_mock.head(pattern, text=mocker_text_callback)
+    requests_mock.get(pattern, text=mocker_text_callback)
+    requests_mock.post(pattern, text=mocker_text_callback)
+
+    # Configure the fail mock handlers; these rely on the mocker to throw the configured exception
+    pattern = re.compile(
+        neptune.replace("http://", "mock-fail://").replace("/sparql", "/(.*)")
+    )
+
+    requests_mock.head(pattern, exc=requests.exceptions.ConnectionError)
+    requests_mock.get(pattern, exc=requests.exceptions.ConnectionError)
+    requests_mock.post(pattern, exc=requests.exceptions.ConnectionError)
+
+    yield requests_mock
--- source/web-service/tests/test_routes_ingest.py
+++ source/web-service/tests/test_routes_ingest.py
@@ -1,73 +1,7 @@
 import json
-import pytest
-import re
-import requests
-import urllib.parse
 
 from flask import current_app
 from flaskapp.routes.ingest import process_neptune_record_set
-
-
-@pytest.fixture()
-def requests_mocker(requests_mock):
-    """The `requests_mocker()` method supports mocking requests to Neptune, which is inaccessible
-    from within CircleCI. The `requests_mocker()` method provides support for mocking successful
-    HTTP requests to Neptune, and providing appropriate responses for the limited set of queries
-    performed by the /ingest endpoint's `process_neptune_record_set()` method, as well as support
-    for generating failed requests to mimic networking issues or connection time-outs."""
-
-    def mocker_text_callback(request, context):
-        if request.path_url == "/status":
-            context.status_code = 200
-            return json.dumps({"status": "healthy",})
-        elif request.path_url == "/sparql":
-            sparql = None
-
-            if request.body.startswith("query=") or request.body.startswith("update="):
-                params = urllib.parse.parse_qsl(request.body)
-                if params:
-                    for param in params:
-                        if param[0] == "query" or param[0] == "update":
-                            sparql = param[1]
-                            break
-
-            if sparql:
-                if sparql.startswith("SELECT"):
-                    context.status_code = 200
-                    return json.dumps(
-                        {"results": {"bindings": [{"count": {"value": 0,}}],},}
-                    )
-                elif sparql.startswith("INSERT DATA"):
-                    context.status_code = 200
-                    return None
-                elif sparql.startswith("DROP GRAPH"):
-                    context.status_code = 200
-                    return None
-
-        context.status_code = 400
-        return None
-
-    # Configure the good mock handlers; these rely on the `mocker_text_callback()` method defined above
-    neptune = current_app.config["NEPTUNE_ENDPOINT"]
-    pattern = re.compile(
-        neptune.replace("http://", "mock-pass://").replace("/sparql", "/(.*)")
-    )
-
-    requests_mock.head(pattern, text=mocker_text_callback)
-    requests_mock.get(pattern, text=mocker_text_callback)
-    requests_mock.post(pattern, text=mocker_text_callback)
-
-    # Configure the fail mock handlers; these rely on the mocker to throw the configured exception
-    neptune = current_app.config["NEPTUNE_ENDPOINT"]
-    pattern = re.compile(
-        neptune.replace("http://", "mock-fail://").replace("/sparql", "/(.*)")
-    )
-
-    requests_mock.head(pattern, exc=requests.exceptions.ConnectionError)
-    requests_mock.get(pattern, exc=requests.exceptions.ConnectionError)
-    requests_mock.post(pattern, exc=requests.exceptions.ConnectionError)
-
-    yield requests_mock
 
 
 class TestIngestErrors:
@@ -216,9 +150,7 @@ class TestIngestSuccess:
 
 
 class TestNeptuneConnection:
-    def test_neptune_connection_good(
-        self, client, namespace, auth_token, requests_mocker
-    ):
+    def test_neptune_connection_good(self, client, namespace, auth_token):
         endpoint = current_app.config["NEPTUNE_ENDPOINT"]
         records = [
             json.dumps(
@@ -230,9 +162,7 @@ class TestNeptuneConnection:
         )
         assert asserted == True
 
-    def test_neptune_connection_fail(
-        self, client, namespace, auth_token, requests_mocker
-    ):
+    def test_neptune_connection_fail(self, client, namespace, auth_token):
         endpoint = current_app.config["NEPTUNE_ENDPOINT"]
         records = [
             json.dumps(

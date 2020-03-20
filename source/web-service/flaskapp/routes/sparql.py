6ab9d200b51225294784771d04c32eb9c0a89ac7
Gregg Garcia <GGarcia@getty.edu>
2020-03-20 11:22:41 -0700

    Added accept parameter and error checking.

--- source/web-service/flaskapp/routes/sparql.py
+++ source/web-service/flaskapp/routes/sparql.py
@@ -3,7 +3,7 @@ import requests
 
 from flask import Blueprint, current_app, request, abort, jsonify
 
-from flaskapp.errors import status_neptune_error
+from flaskapp.errors import status_nt, status_neptune_error, construct_error_response
 
 # Create a new "sparql" route blueprint
 sparql = Blueprint("sparql", __name__)
@@ -17,14 +17,28 @@ def query_entrypoint():
     else:
         query = request.form.get("query")
 
-    accept_header = request.headers["Accept"]
-    current_app.logger.info(accept_header)
+    if query is None:
+        response = construct_error_response(
+            status_nt(400, "Query error", "No query parameter included")
+        )
+        return abort(response)
+
+    accept_header = None
+    if "accept" in request.args:
+        accept_header = request.args["accept"]
+    else:
+        accept_header = request.form.get("accept")
+
+    if accept_header is None:
+        accept_header = request.headers["Accept"]
+        current_app.logger.info(accept_header)
 
     neptune_endpoint = current_app.config["NEPTUNE_ENDPOINT"]
     try:
         res = execute_query(query, accept_header, neptune_endpoint)
         return res
     except requests.exceptions.ConnectionError as e:
+        response = construct_error_response(status_neptune_error)
         return status_neptune_error
 
 

866cd312fb1cb7880e02e665608d3c7d702a88a4
Gregg Garcia <GGarcia@getty.edu>
2020-03-20 10:44:15 -0700

    Added basic query capability. DEV-4190

---
+++ source/web-service/flaskapp/routes/sparql.py
@@ -0,0 +1,35 @@
+import json
+import requests
+
+from flask import Blueprint, current_app, request, abort, jsonify
+
+from flaskapp.errors import status_neptune_error
+
+# Create a new "sparql" route blueprint
+sparql = Blueprint("sparql", __name__)
+
+# ### ROUTES ###
+@sparql.route("/sparql", methods=["GET", "POST"])
+def query_entrypoint():
+    query = None
+    if "query" in request.args:
+        query = request.args["query"]
+    else:
+        query = request.form.get("query")
+
+    accept_header = request.headers["Accept"]
+    current_app.logger.info(accept_header)
+
+    neptune_endpoint = current_app.config["NEPTUNE_ENDPOINT"]
+    try:
+        res = execute_query(query, accept_header, neptune_endpoint)
+        return res
+    except requests.exceptions.ConnectionError as e:
+        return status_neptune_error
+
+
+def execute_query(query, accept_header, neptune_endpoint):
+    res = requests.post(
+        neptune_endpoint, data={"query": query}, headers={"Accept": accept_header}
+    )
+    return res.content

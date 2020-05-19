07ac53dc19f30fb906b27ca0d439c7adc8afbb52
Apau, Emmanuel <e@mechanicode.io>
2020-05-18 20:26:35 -0400

    migrate ecr_profile setup to makefile
    corrected aws acc num, and deleted repo check/create script
    
    added image confirmation step

--- aws/config_profiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws configure set aws_access_key_id $ECR_ACCESS_KEY_ID --profile getty
-aws configure set aws_secret_access_key $ECR_SECRET_ACCESS_KEY --profile getty
-aws configure set region $ECR_REGION --profile getty
-
-aws configure list --profile getty

42342e0c4e6086e6fbf6a3be0316c258fe0f91ab
benosteen <bosteen@gmail.com>
2020-09-17 15:37:35 -0700

    Supports the 'If-None-Match' ETag response. If the client supplied etag matches the record's stored etag, then there has been no change since the client last requested the item. A 304 Not Modified response is returned with no data.

--- source/web-service/flaskapp/routes/records.py
+++ source/web-service/flaskapp/routes/records.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 
-from flask import Blueprint, current_app, abort
+from flask import Blueprint, current_app, abort, request
 
 from flaskapp.models.record import Record
 from flaskapp.utilities import format_datetime, containerRecursiveCallback, idPrefixer
@@ -18,6 +18,19 @@ def entity_record(entity_id):
 
     # if data == None, the record was deleted
     if record and record.data:
+        current_app.logger.debug(request.if_none_match)
+        if record.checksum in request.if_none_match:
+            # Client has supplied etags of the resources it has cached for this URI
+            # If the current checksum for this record matches, send back an empty response
+            # using HTTP 304 Not Modified, with the etag and last modified date in the headers
+            headers = {
+                "Last-Modified": format_datetime(record.datetime_updated),
+                "etag": record.checksum,
+            }
+            return ("", 304, headers)
+
+        # If the etag(s) did not match, then the record is not cached or known to the client
+        # and should be sent:
 
         # Assemble the record 'id' attribute base URL prefix
         idPrefix = (
@@ -26,11 +39,12 @@ def entity_record(entity_id):
 
         # Recursively prefix each 'id' attribute that currently lacks a http(s):// prefix
         data = containerRecursiveCallback(
-            data=record.data, attr="id", callback=idPrefixer, prefix=idPrefix,
+            data=record.data, attr="id", callback=idPrefixer, prefix=idPrefix
         )
 
         response = current_app.make_response(data)
         response.headers["Last-Modified"] = format_datetime(record.datetime_updated)
+        response.headers["etag"] = record.checksum
         return response
     else:
         response = construct_error_response(status_record_not_found)

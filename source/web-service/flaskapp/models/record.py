3bd287e691bccf79d165991a87c8ae3c47f7a11b
Gregg Garcia <ggarcia@getty.edu>
2021-05-25 09:54:19 -0700

    Alembic migration for index on entity type

--- source/web-service/flaskapp/models/record.py
+++ source/web-service/flaskapp/models/record.py
@@ -5,7 +5,7 @@ class Record(db.Model):
     __tablename__ = "records"
     id = db.Column(db.Integer, primary_key=True)
     entity_id = db.Column(db.String, nullable=False, index=True, unique=True)
-    entity_type = db.Column(db.String)
+    entity_type = db.Column(db.String, index=True)
     datetime_created = db.Column(db.TIMESTAMP, nullable=False)
     datetime_updated = db.Column(db.TIMESTAMP, nullable=False)
     datetime_deleted = db.Column(db.TIMESTAMP)

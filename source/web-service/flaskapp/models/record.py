eb3d3657205576b476d84203ca751e4cb45705ef
Ben O'Steen <bosteen@getty.edu>
2022-03-09 16:50:04 -0800

    Adding the base models for the versioning

--- source/web-service/flaskapp/models/record.py
+++ source/web-service/flaskapp/models/record.py
@@ -1,6 +1,7 @@
 from flaskapp.models import db
+from sqlalchemy import ForeignKey
 
-
+# Keeping the old columns to allow for migration
 class Record(db.Model):
     __tablename__ = "records"
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +14,22 @@ class Record(db.Model):
     previous_version = db.Column(db.String, nullable=True, index=True)
     is_old_version = db.Column(db.Boolean, nullable=True, default=False)
     checksum = db.Column(db.String, nullable=True)
+
+
+class Version(db.Model):
+    __tablename__ = "versions"
+    id = db.Column(db.Integer, primary_key=True)
+    entity_id = db.Column(db.String, nullable=False, index=True, unique=True)
+    entity_type = db.Column(db.String, index=True)
+    datetime_created = db.Column(db.TIMESTAMP, nullable=False, index=True)
+    datetime_updated = db.Column(db.TIMESTAMP, nullable=False, index=True)
+    datetime_deleted = db.Column(db.TIMESTAMP)
+    data = db.Column(db.JSON)
+    checksum = db.Column(db.String, nullable=True)
+    record_id = db.Column(db.Integer, ForeignKey("records.id"))
+    record = db.relationship(
+        "Record",
+        backref=db.backref(
+            "versions", lazy=True, order_by="Version.datetime_updated.desc()"
+        ),
+    )

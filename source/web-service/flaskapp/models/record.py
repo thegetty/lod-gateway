0dedf5279d3785dcd21aa2886f842d61450b7e3b
BOsteen <bosteen@getty.edu>
2022-06-30 12:03:19 -0700

    Tweaking the indexes for the version and record tables

--- source/web-service/flaskapp/models/record.py
+++ source/web-service/flaskapp/models/record.py
@@ -11,21 +11,20 @@ class Record(db.Model):
     entity_type = db.Column(db.String, index=True)
     datetime_created = db.Column(db.TIMESTAMP, nullable=False, index=True)
     datetime_updated = db.Column(
-        DateTime(timezone=True),
+        DateTime(),
         onupdate=func.now(),
         server_default=func.now(),
         index=True,
+        nullable=False,
     )
     datetime_deleted = db.Column(db.TIMESTAMP)
     data = deferred(db.Column(db.JSON))
-    previous_version = db.Column(db.String, nullable=True, index=True)
+    previous_version = db.Column(db.String, nullable=True)
     is_old_version = db.Column(db.Boolean, nullable=True, default=False)
     checksum = db.Column(db.String, nullable=True)
 
 
-Index(
-    "ix_records_record_id_and_type", Record.entity_id, Record.entity_type, unique=True
-)
+Index("ix_records_record_id_and_type", Record.entity_id, Record.entity_type)
 
 
 class Version(db.Model):
@@ -33,7 +32,7 @@ class Version(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     entity_id = db.Column(db.String, nullable=False, index=True, unique=True)
     entity_type = db.Column(db.String, index=True)
-    datetime_created = deferred(db.Column(db.TIMESTAMP, nullable=False, index=True))
+    datetime_created = deferred(db.Column(db.TIMESTAMP, nullable=False))
     datetime_updated = db.Column(
         DateTime(timezone=True),
         onupdate=func.now(),
@@ -50,3 +49,13 @@ class Version(db.Model):
             "versions", lazy="select", order_by="Version.datetime_updated.desc()"
         ),
     )
+
+
+Index(
+    "ix_versions_chronological_lookup",
+    Version.record_id,
+    Version.datetime_updated,
+    Version.entity_id,
+    postgresql_using="btree",
+    postgresql_ops={"datetime_updated": "DESC"},
+)

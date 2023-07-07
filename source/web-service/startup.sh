994eebd963e2cf20d16a716dfed191d28b7878f3
BOsteen <bosteen@getty.edu>
2023-07-07 16:18:21 -0700

    Allowing gevent as a possible web worker for gunicorn

--- source/web-service/startup.sh
+++ source/web-service/startup.sh
@@ -2,17 +2,37 @@
 
 flask db upgrade 
 
+# Worker type
+WORKER_CLASS="${WORKER_CLASS:-sync}"
+
+# Number of threads
 THREADS="${WEB_THREADS:-1}"
 
 # Workers rule of thumb -> 2 to 4 X (number of cores available)
 WORKERS="${WEB_WORKERS:-3}"
+WORKER_CONNECTIONS="${WORKER_CONNECTIONS:-1000}"
 
 FLASK_RUN_PORT="${FLASK_RUN_PORT:-5100}"
 
-gunicorn --bind 0.0.0.0:${FLASK_RUN_PORT} \
+# sync/threads? or green threads?
+if [[ "$WORKER_CLASS" != "gevent" ]]; then
+	echo "Running with either sync/gthreads - $WORKERS workers, $THREADS threads" && \
+    gunicorn --bind 0.0.0.0:${FLASK_RUN_PORT} \
          --threads ${THREADS} \
          --workers ${WORKERS} \
+         --worker-class ${WORKER_CLASS} \
          --access-logfile '-' \
          --error-logfile '-' \
          wsgi:app
 	$@
+else
+	echo "Running with gevents - $WORKERS workers, $WORKER_CONNECTIONS connections" && \
+    gunicorn --bind 0.0.0.0:${FLASK_RUN_PORT} \
+         --worker-connections ${WORKER_CONNECTIONS} \
+         --workers ${WORKERS} \
+         --worker-class ${WORKER_CLASS} \
+         --access-logfile '-' \
+         --error-logfile '-' \
+         wsgi:app
+	$@
+fi

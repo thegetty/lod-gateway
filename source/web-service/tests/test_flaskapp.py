5a53bc8605d6c0531427c040614e53312fd42f7c
ViktorGetty <vgrabarchuk@getty.edu>
2023-09-05 10:03:08 -0700

    sample data added to test

--- source/web-service/tests/test_flaskapp.py
+++ source/web-service/tests/test_flaskapp.py
@@ -1,8 +1,8 @@
 class TestFlaskApp:
-    # def test_home_page(self, client, namespace):
-    #     response = client.get(f"/{namespace}/")
-    #     assert response.status_code == 200
-    #     assert b"LOD Gateway" in response.data
+    def test_home_page(self, sample_data, client, namespace):
+        response = client.get(f"/{namespace}/")
+        assert response.status_code == 200
+        assert b"LOD Gateway" in response.data
 
     def test_cors_response(self, client, namespace):
         response = client.options(f"/{namespace}/")

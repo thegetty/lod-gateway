2475d178d93d6ac4bd5b767d2bfc4ae0565f9cba
ViktorGetty <vgrabarchuk@getty.edu>
2023-09-18 15:49:04 -0700

    fix for black

--- source/web-service/flaskapp/routes/home_page.py
+++ source/web-service/flaskapp/routes/home_page.py
@@ -85,10 +85,10 @@ def get_last_modified_date():
     return date
 
 
-def get_version(): 
-    f_name = "version.txt"  
+def get_version():
+    f_name = "version.txt"
     dir_path = os.path.join(os.pardir, f_name)
-   
+
     try:
         with open(dir_path) as f:
             return f.read()

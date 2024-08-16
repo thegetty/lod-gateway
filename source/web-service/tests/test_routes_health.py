07e8baea3efc8286aa35ddb45b68719ba9b1b2b5
bosteen@getty.edu <bosteen@getty.edu>
2024-08-16 13:28:23 -0700

    Adding test for new auth healthcheck endpoint

--- source/web-service/tests/test_routes_health.py
+++ source/web-service/tests/test_routes_health.py
@@ -3,6 +3,13 @@ class TestHealthRoute:
         response = client.get(f"/{namespace}/health")
         assert response.status_code == 200
 
+    def test_auth_health_ok(self, client, namespace, test_db, auth_token):
+        response = client.get(
+            f"/{namespace}/authhealth",
+            headers={"Authorization": "Bearer " + auth_token},
+        )
+        assert response.status_code == 200
+
     def test_health_db_down(self, client, namespace, test_db):
         test_db.drop_all()
         response = client.get(f"/{namespace}/health")
